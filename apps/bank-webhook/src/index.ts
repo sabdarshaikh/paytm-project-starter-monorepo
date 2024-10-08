ffe1b79bc47342499abe131079db63589a97c32f
sabdar <sabdarshaikh10@gmail.com>
2024-10-08 11:42:11 +0530

    created web hook updated the db and added onRamp Transaction

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,34 @@
+import express from "express"
+import db from "@repo/db/client"
+const app = express()
+
+app.post("/hdfcWebhook", (req, res) => {
+  //TODO: Add zod validation here?
+  const paymentInformation = {
+    token: req.body.token,
+    userId: req.body.user_identifier,
+    amount: req.body.amount,
+  }
+  db.balance.update({
+    where: {
+      userId: paymentInformation.userId,
+    },
+    data: {
+      amount: {
+        increment: paymentInformation.amount,
+      },
+    },
+  })
+  db.onRampTransaction.update({
+    where: {
+      token: paymentInformation.token,
+    },
+    data: {
+      status: "Success",
+    },
+  })
+  res.json({
+    message: "captured",
+  })
+  // Update balance in db, add txn
+})
